621559c331e3edfd91298ab59664345798f1a8af
Jacob <Jacob.pahlsson@hotmail.com>
2019-09-30 08:45:41 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,38 @@ namespace Restaurant
     {
         static void Main(string[] args)
         {
-            
+
+
+            bool shouldNotExit = true;
+
+            Console.Clear();
+
+            while (shouldNotExit)
+            {
+                Console.WriteLine("1. ");
+                Console.WriteLine("2. ");
+                Console.WriteLine("3. ");
+
+                int userInput = int.Parse(Console.ReadLine());
+
+                switch (userInput)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        shouldNotExit = false;
+                        break;
+
+
+
+                }
+
+                Console.Clear();
+            }
         }
     }
 }
